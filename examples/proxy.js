04d39e04f52491b4a0d902baa15ea876ec3bd28b
Björn Ramberg <bjorn@burtcorp.com>
2012-01-07 14:23:57 +0100

    Convert to connect middleware and update proxy example.

--- examples/proxy.js
+++ examples/proxy.js
@@ -1,21 +1,23 @@
-var proximity = require('proximity');
+var proximity = require('../../proximity'),
+    connect   = require('connect');
 
-var prox = proximity.createServer({ port: 8004 });
+var proxy = proximity.proxy();
+var server = connect.createServer(
+  connect.logger(),
+  proxy.middleware()
+).listen(8004);
 
-// DSL 1
-
-prox.setRules([
-  // {
-  //   type: 'request',
-  //   match: { url: 'localhost' },
-  //   url: 'http://localhost:3000/',
-  //   body: 'hej'
-  // },
+proxy.setRules([
+  proximity.rules.noCompression,
+  proximity.rules.noCache,
   {
+    // paragraph count
+    // e.g.
+    // <p>hello world</p> => <p>hello world (11)</p>
     type: 'response',
-    match: { url: 'localhost' },
+    match: { url: /wikipedia.org/ },
     body: function(body) {
-      return body.replace(/>agent</g, '>SKOJARE<');
+      return body.replace(/(<p>)(.+)(<\/p>)/gm, function(s,pre,str,post) { return pre + str + ' (' + str.length + ')' + post; });
     }
   }
 ]);
