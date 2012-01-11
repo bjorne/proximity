4a2add4998a715e7d0b1f60283bfa6dedb5ee488
Björn Ramberg <bjorn@burtcorp.com>
2012-01-11 19:24:42 +0100

    Add rules names and getRules.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Proximity
+
+Proximity is a rewriting reverse HTTP proxy.
+
+# Examples
+
+* Word mangler
+* Image transformer (replace all images with lolcats?)
--- examples/proxy.js
+++ examples/proxy.js
@@ -1,11 +1,16 @@
-var proximity = require('../../proximity'),
-    connect   = require('connect');
+var proximity   = require('../../proximity'),
+    proximityUi = require('../../proximity-ui'),
+    connect     = require('connect');
 
 var proxy = proximity.proxy();
 var server = connect.createServer(
   connect.logger(),
   proxy.middleware()
 ).listen(8004);
+
+var uiServer = proximityUi.createServer({
+  proxy: proxy
+}).listen(4000);
 
 proxy.setRules([
   proximity.rules.noCompression,
@@ -14,6 +19,7 @@ proxy.setRules([
     // paragraph count
     // e.g.
     // <p>hello world</p> => <p>hello world (11)</p>
+    name: 'Wikipedia paragraph count',
     type: 'response',
     match: { url: /wikipedia.org/ },
     body: function(body) {
